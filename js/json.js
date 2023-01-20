608f65941498d96b515e20ece0048bce650954ce
mouhamed <coulibalymouhamed27@gmail.com>
2023-01-20 12:00:25 +0000

    depot du fichier json

---
+++ js/json.js
@@ -0,0 +1,38 @@
+let myJSON =
+[
+    {
+      "nom": "coulibaly",
+      "prenom":"mouhamed" ,
+      "date_naissance": "01/01/2022",
+      "mail": "Dan Jukes",
+      
+    },
+    {
+        "nom": "coulibaly",
+        "prenom":"mouhamed" ,
+        "date_naissance": "01/01/2022",
+        "mail": "Dan Jukes",
+        
+      },
+      {
+        "nom": "coulibaly",
+        "prenom":"mouhamed" ,
+        "date_naissance": "01/01/2022",
+        "mail": "Dan Jukes",
+        
+      },
+      {
+        "nom": "coulibaly",
+        "prenom":"mouhamed" ,
+        "date_naissance": "01/01/2022",
+        "mail": "Dan Jukes",
+        
+      },
+      {
+        "nom": "coulibaly",
+        "prenom":"mouhamed" ,
+        "date_naissance": "01/01/2022",
+        "mail": "Dan Jukes",
+        
+      }
+  ]
